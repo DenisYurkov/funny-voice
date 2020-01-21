f5dafbcd3814c51cd7433a0d00bef935ced4a10d
DenisYukov <denisdeveloper136@gmail.com>
2020-01-21 23:04:04 +0300

    add project funny_voice

---
+++ funny_voice.py
@@ -0,0 +1,35 @@
+""" 
+funny_voice.py
+Create: Denis Yurkov.
+"""
+
+# import models.
+import pyautogui
+import pyttsx3
+
+# init.
+engine = pyttsx3.init()
+
+# getting details of current voice.
+voices = engine.getProperty('voices')
+
+# changing index, changes voices. 1 for female, on russian use 2.
+engine.setProperty('voice', voices[2].id)
+
+start_program = True
+while start_program:
+    
+    # gui.
+    gui_message = pyautogui.alert(text='Введите предложение или слова, и послушайте изменения его голоса.', title='Funny Voice')
+    gui_prompt = pyautogui.prompt(text='Введите слова или предложение для озвучки: ', title='Funny Voice')
+
+    # logic.
+    if gui_prompt:
+        engine.say(gui_prompt)
+        engine.runAndWait()
+        
+        gui_exit = pyautogui.confirm(text='Хотите ли вы выйти?', title='Funny Voice')
+        if gui_exit == "OK":
+            start_program = False
+        else:
+            start_program = True
